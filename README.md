4ee09b73387fa34384270c4860c41f6250659911
bergwhite <bergwhites@gmail.com>
2017-05-21 12:05:49 +0800

    add REST API

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # NodeJS & SocketIO & Express & & EJS MongoDB 打造多人在线聊天室
 
+项目开发中...
+
+> REST API
+
+```
+
+/  // 首页
+/user  // 用户列表
+/user/:id  // 用户中心
+/user/:id/register  // 注册
+/user/:id/login  // 登陆
+/user/:id/logout  // 注销
+/user/:id/info  // 用户资料
+
+```
+
 > 启动MongoDB服务
 
 首先修改package.json中的MongoDB安装路径，`--dbpath=`后面的路径修改为自己的即可。
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,23 @@ var user = require('../database').user;
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/user', function(req, res, next) {
+  res.render('user', { title: req.params.id });
+});
+router.get('/user/:id', function(req, res, next) {
+  res.render('user', { title: req.params.id });
+});
+router.get('/user/:id/register', function(req, res, next) {
+  res.send({status: "success"});
+});
+router.get('/user/:id/login', function(req, res, next) {
+  res.send({status: "success"});
+});
+router.get('/user/:id/logout', function(req, res, next) {
+  res.send({status: "success"});
+});
+router.get('/user/:id/info', function(req, res, next) {
+  res.send({user: "admin"});
+});
 
 module.exports = router;
