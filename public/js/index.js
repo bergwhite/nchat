bb22f87ae1641523930b66c14fcc9117d3c82032
bergwhite <bergwhites@gmail.com>
2017-05-22 18:28:26 +0800

    beautiful the code for socket

--- public/js/index.js
+++ public/js/index.js
@@ -1,76 +1,145 @@
-var pathName = document.location.pathname
-var isHome = pathName === '/'
-var roomId = null
-if (!isHome && (pathName.indexOf('room') !== -1)) {
-  roomId = pathName.replace(/\/.*?\//,'')
-}
-roomId === null ? roomId = '@main@' : ''
-console.log('roomId: ' + roomId)
-var currentUser = null
-var newUser = document.getElementById('newUser')
-var tip = document.getElementById('tip')
-var chatLists = document.getElementsByClassName('chat-lists')[0]
-var chatSendCtx = document.getElementsByClassName('chat-send-ctx')[0]
-var chatShowCtx = document.getElementsByClassName('chat-show-ctx')[0]
-function sendMessage () {
-  socket.emit('sendMessage', roomId , {user: currentUser !== null ? currentUser : '神秘人', msg: chatSendCtx.innerHTML})
-  appendElement(chatShowCtx, 'li', (currentUser !== null ? currentUser : '神秘人') + ':' + chatSendCtx.innerHTML)
-}
-function renderUserList (arr) {
-  for(var i = 0; i < arr.length; i++){
-    appendElement(chatLists, 'li', arr[i])
-  }
-}
-function initDOMFragement (type) {
-  type['innerHTML'] = ''
-}
-function appendElement (parentDOM, childType, childCtx) {
-  var childDOM = document.createElement(childType)
-  childDOM.innerHTML = childCtx
-  parentDOM.appendChild(childDOM)
-}
-var socket = io('http://localhost:81/')
-socket.on('request room id', function () {
-  socket.emit('response room id', roomId)
-})
-socket.on('init the room', function (data) {
-  appendElement(chatShowCtx, 'li', data.user + ':' + decodeURIComponent(data.msg))
-})
-socket.on('latestTalk', function (data) {
-  appendElement(chatShowCtx, 'li', data.user + ':' + data.msg)
-  console.log(data)
-})
-socket.on('welcome', function  (data) {
-  initDOMFragement(chatLists)
-  renderUserList(data.userList)
-  console.log(data)
-  console.log("当前在线：" + data.userList.length)
-})
-socket.on('renderOnlineList', function (data) {
-  appendElement(chatLists, 'li', data)
-})
-socket.on('showUser', function  (data) {
-  if (!data.status) {
-    tip.innerHTML = '用户名已存在'
-    currentUser = null
-  } else {
-    tip.innerHTML = '注册成功'
-    renderUserList([data.userList[data.userList.length -1 ]])
-  }
-  console.log(data)
-  console.log("当前在线：" + data.userList.length)
-  console.log("注册状态：" + data.status)
-})
-function reg () {
-  if (currentUser) {
-    tip.innerHTML = '已登陆，用户名为：' + currentUser
-  }else if (newUser.value !== "" && newUser.value !== " ") { 
-    currentUser = newUser.value
-    socket.emit('user', {newUserName: newUser.value})
-  } else {
-    tip.innerHTML = '请输入用户名'
-  }
-}
+var userList = document.getElementsByClassName('user-lists')[0]
+var userReg = document.getElementById('user-reg')
+var userRegTip = document.getElementById('user-reg-tip')
+var chatMsgList = document.getElementsByClassName('chat-msg-list')[0]
+var chatMsgSend = document.getElementsByClassName('chat-msg-send')[0]
+
+// 文档加载完毕自动在输入框获得焦点
 document.body.onload = function () {
-  chatSendCtx.focus()
+  chatMsgSend.focus()
 }
+
+// 为socket.io设置别名
+var socket = io('http://localhost:81/')
+
+// 把聊天室所有的操作封装在命名空间内
+var nodejsChat = {}
+
+// 数据（存放变量）
+nodejsChat.data = {
+  isRoomInit: false,
+  // 房间ID
+  roomID: null,
+  // 用户资料
+  user: {
+    name: null,
+    pass: null,
+    desc: null,
+    img: null
+  }
+}
+// 房间（socket通讯）
+nodejsChat.room = {
+  // 初始化
+  init: function () {
+    socket.on('request room id', function () {
+      socket.emit('response room id', nodejsChat.data.roomID)
+    })
+    socket.on('init the room', function (data) {
+      nodejsChat.method.appendElement(chatMsgList, 'li', data.user + ': ' + decodeURIComponent(data.msg))
+    })
+  },
+  // 渲染
+  render: function () {
+    // 进入页面打印当前聊天室状态
+    socket.on('current status', function  (data) {
+      nodejsChat.method.initDOMFragement(userList)
+      nodejsChat.method.renderUserList(nodejsChat.method.filterVal(data.room, nodejsChat.data.roomID))
+      console.log(data)
+      console.log("当前在线：" + nodejsChat.method.filterVal(data.room, nodejsChat.data.roomID).length)
+    })
+    // 渲染在线用户列表
+    socket.on('renderOnlineList', function (data) {
+      nodejsChat.method.appendElement(userList, 'li', data)
+    })
+    // 把最新的消息添加进DOM
+    socket.on('latestTalk', function (data) {
+      nodejsChat.method.appendElement(chatMsgList, 'li', data.user + ': ' + data.msg)
+      console.log(data)
+    })
+    //
+    socket.on('showUser', function  (data) {
+      if (!data.addUserStatus) {
+        userRegTip.innerHTML = '用户名已存在'
+        nodejsChat.data.user.name = null
+      } else {
+        userRegTip.innerHTML = '注册成功'
+        nodejsChat.method.renderUserList([data.user[data.user.length -1 ]])
+      }
+      console.log(data)
+      console.log("当前在线：" + data.user.length)
+      console.log("注册状态：" + data.status)
+    })
+  },
+  logout: function () {
+    socket.on('request logout user', function () {
+      console.log('request logout user')
+      socket.emit('response logout user', nodejsChat.data.user.name)
+    })
+  }
+}
+// 方法（存放函数）
+nodejsChat.method = {
+  // 获取房间ID
+  getRoomID: function () {
+    var pathName = document.location.pathname
+    var isHome = pathName === '/'
+    var roomId = null
+    if (!isHome && (pathName.indexOf('room') !== -1)) {
+      roomId = pathName.replace(/\/.*?\//,'')
+    }
+    return roomId === null ? roomId = 'Chat Room' : roomId
+  },
+  // 渲染用户列表
+  renderUserList: function (arr) {
+    for(var i = 0; i < arr.length; i++){
+      this.appendElement(userList, 'li', arr[i])
+    }
+  },
+  // 发送消息
+  sendMessage: function () {
+    socket.emit('send message', nodejsChat.data.roomID , {user: nodejsChat.data.user.name !== null ? nodejsChat.data.user.name : '神秘人', msg: chatMsgSend.innerHTML})
+    nodejsChat.method.appendElement(chatMsgList, 'li', (nodejsChat.data.user.name !== null ? nodejsChat.data.user.name : '神秘人') + ':' + chatMsgSend.innerHTML)
+  },
+  // 注册用户
+  regUser: function () {
+    if (nodejsChat.data.user.name) {
+      userRegTip.innerHTML = '已登陆，用户名为：' + nodejsChat.data.user.name
+    }else if (userReg.value !== "" && userReg.value !== " ") { 
+      nodejsChat.data.user.name = userReg.value
+      socket.emit('add user', nodejsChat.data.roomID, {name: userReg.value})
+    } else {
+      userRegTip.innerHTML = '请输入用户名'
+    }
+  },
+  // 清空节点内容
+  initDOMFragement: function (type) {
+    type['innerHTML'] = ''
+  },
+  // 在节点下添加子节点
+  appendElement: function (parentDOM, childType, childCtx) {
+    var childDOM = document.createElement(childType)
+    childDOM.innerHTML = childCtx
+    parentDOM.appendChild(childDOM)
+  },
+  filterVal: function (arr, type) {
+    var result =  arr.filter(function (val) {
+      console.log(val.name)
+      console.log(val.user)
+      if (val.name === type) {
+        console.log('xxxxxx')
+      }
+    })
+    return result
+  }
+}
+
+// 初始化房间ID
+nodejsChat.data.roomID = nodejsChat.method.getRoomID()
+
+// 初始化
+// 为当前房间分配ID
+nodejsChat.room.init()
+// 渲染
+nodejsChat.room.render()
+nodejsChat.room.logout()
--- public/js/socket-server.js
+++ public/js/socket-server.js
@@ -1,55 +1,97 @@
 const app = require('express')();
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const fs = require('fs')
-// 在线用户
-var count = 0;
-// 用户列表
-var userList = [];
 
-var logonList = [];
-// 房间清单
-var roomList = []
-var status = null;
+var chat = {}
+
+// 数据
+chat.data = {
+  // 所有在线用户
+  user: [],
+  // 所有聊天室
+  room: [{
+    // 名字
+    name: 'Chat Room',
+    // 描述
+    desc: null
+    // 用户
+    user: []
+  }],
+  // 添加用户的状态
+  addUserStatus: null
+}
+
+// 方法
+chat.method = {
+  // 判断当前房间是否存在
+  isRoomExist: function (arr, roomID) {
+    var a = arr.filter(function(val){
+      if(val.name === roomID) {
+        console.log(val.name)
+        return true
+      }
+    })
+    return a.length !== 0
+  },
+  // 添加用户到指定房间
+  addUserToRoom: function (name, roomID) {
+    chat.data.room.findIndex(function(val,index){
+      if(val.name === roomID) {
+        chat.data.room[index].user.push(name)
+      }
+    })
+  }
+}
+
+// socket链接时执行
 io.on('connection', function (socket) {
-  console.log('One User Is Online')
+  // 发送请求当前房间号事件
   socket.emit('request room id')
-  socket.on('response room id', function (data) {
-    if (roomList.indexOf(data) === -1) {
-      roomList.push(data)
+  // 监听到相应后，存储当前的房间号
+  socket.on('response room id', function (roomID) {
+    // 不存在则创建新房间
+    if(!chat.method.isRoomExist(chat.data.room, roomID)) {
+      chat.data.room.push({
+        name: roomID,
+        desc: null,
+        user: []
+      })
     }
-    socket.join(data)
-    io.to(data).emit('init the room', {
+    // 进入房间
+    socket.join(roomID)
+    // 为当前房间发送欢迎消息
+    io.to(roomID).emit('init the room', {
       user: 'system',
-      msg: 'welcome to the ' + data
+      msg: 'welcome to the ' + roomID
     });
   })
-  socket.emit('welcome', {hello: 'world', userList: userList, logonList: logonList})
-  socket.on('user', function (data) {
-    if(userList.indexOf(data.newUserName) === -1) {
-      userList.push(data.newUserName)
-      console.log(data.newUserName)
-      status = true
-      logonList.push(data.newUserName)
-      socket.broadcast.emit('renderOnlineList', data.newUserName)
+  // 添加用户
+  socket.on('add user', function (id, msg) {
+    chat.method.addUserToRoom(msg.name,id)
+    console.log(msg)
+    if(chat.data.user.indexOf(msg.name) === -1) {
+      chat.data.user.push(msg.name)
+      chat.data.addUserStatus = true
+      socket.broadcast.to(id).emit('renderOnlineList', msg.name)
     } else {
-      console.log(data.newUserName + 'is exist.')
-      status = false
+      chat.data.addUserStatus = false
     }
-    socket.emit('showUser', {status: status, userList: userList, logonList: logonList})
+    socket.emit('showUser', chat.data)
   })
-  // io.emit('this', { will: 'be received by everyone'});
-  // socket.on('private message', function (from, msg) {
-  //   console.log('I received a private message by ', from, ' saying ', msg);
-  // });
-  socket.on('sendMessage', function (id, data) {
-    socket.broadcast.to(id).emit('latestTalk', data)
-    console.log(data)
+  // 给指定房间发送消息
+  socket.on('send message', function (id, msg) {
+    socket.broadcast.to(id).emit('latestTalk', msg)
   })
+  // 显示当前状态
+  socket.emit('current status', chat.data)
+  // 退出连接时的方法
   socket.on('disconnect', function () {
-    console.log('One User Is Offline')
-    socket.emit('user disconnected');
+    socket.emit('request logout user')
+    socket.on('response logout user', function (msg) {
+      console.log(msg + ' is gone.')
+    })
   });
 })
+// 监听81端口
 server.listen(81)
 console.log('socket-server on 81')
