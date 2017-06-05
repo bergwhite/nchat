fd938854c01249d82ce18ebed2e2bc18ec7a18ef
bergwhite <bergwhites@gmail.com>
2017-06-05 10:28:29 +0800

    define the mongodb table

--- database/index.js
+++ database/index.js
@@ -5,4 +5,17 @@ var userSche = new Schema({
   name: String,
   pass: String
 });
+var roomSche = new Schema({
+  name: String,
+  desc: String
+});
+var messSche = new Schema({
+  room: String,
+  user: String,
+  mess: String,
+  time: String
+});
 exports.user = db.model('user', userSche);
+exports.room = db.model('room', roomSche);
+exports.mess = db.model('mess', messSche);
+console.log(userSche);
