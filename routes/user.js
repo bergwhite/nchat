0221f15892783f9454df3747ca3384b708c4bda6
bergwhite <bergwhites@gmail.com>
2017-07-02 22:39:01 +0800

    beautiful the api

--- routes/user.js
+++ routes/user.js
@@ -26,7 +26,9 @@ function isUserLogin(userName) {
   }
 }
 
-router.post('/api/user/:id/register', function(req, res, next) {
+/* 后端API */
+// 注册
+router.post('/api/register', function(req, res, next) {
   var isExist = isUserExist(req.body.name)
   if (!req.body) {
     res.send({msgCode:400, msgCtx: 'Please enter the entire form value.'});
@@ -42,7 +44,8 @@ router.post('/api/user/:id/register', function(req, res, next) {
     res.send({msgCode:1001, msgCtx: 'User is exist.'});
   }
 });
-router.post('/api/user/:id/login', function(req, res, next) {
+// 登陆
+router.post('/api/login', function(req, res, next) {
   if (!req.body) {
     res.send({msgCode:400, msgCtx: 'Please enter the entire form value.'});
   }
@@ -53,13 +56,13 @@ router.post('/api/user/:id/login', function(req, res, next) {
     res.send({msgCode:1011, msgCtx: 'User login fail.'});
   }
 });
-router.post('/api/user/:id/logout', function(req, res, next) {
+// 注销
+router.post('/api/logout', function(req, res, next) {
   res.send({status: "success"});
 });
-// 用户资料路由
+// 个人资料
 router.get('/api/user/:id', function(req, res, next) {
   // 获取用户资料
-  console.log(req)
   info.findOne({user: req.params.id}, function(err,val){
     // 如果当前用户不存在
     // 返回错误信息
@@ -74,5 +77,35 @@ router.get('/api/user/:id', function(req, res, next) {
     console.log('/user/:id/info / err: ' + err)
   })
 });
+
+/* 前端路由 */
+// 个人资料
+/**
+ * {user, gender, img, city, hobbies[]}
+ */
+router.get('/user/:id', function(req, res, next) {
+  info.findOne({user: req.params.id}, function(err,val){
+    if (val === null) {
+      res.send({msgCode:404, msgCtx: 'User not exist.'})
+    }
+    else {
+      res.render('userInfo', {
+        user: val.user,
+        gender: val.gender,
+        img: val.img,
+        city: val.city,
+        hobbies: val.hobbies
+      })
+    }
+  })
+})
+// 登陆页面
+router.get('/login', function(req, res, next) {
+  res.render('userLogin')
+})
+// 注册页面
+router.get('/register', function(req, res, next) {
+  res.render('userRegister')
+})
 
 module.exports = router
