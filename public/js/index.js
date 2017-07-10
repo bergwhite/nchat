449ff979c3ed08f44c04465ae5bfdfb12af7d86b
bergwhite <bergwhites@gmail.com>
2017-07-10 21:08:13 +0800

    add a resize function make it more east to debug

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,18 @@ var chatMoreBox = document.getElementsByClassName('chat-more-box')[0]
 var inputRadioSex = document.getElementsByClassName('input-radio-sex')
 var userImgChoose = document.getElementsByClassName('user-img-choose')[0]
 
-var pageScale = 1 / window.devicePixelRatio;
-document.querySelector('meta[name="viewport"]').setAttribute('content','initial-scale=' + pageScale + ', maximum-scale=' + pageScale + ', minimum-scale=' + pageScale + ', user-scalable=no');
-document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px'
+function startResponse() {
+  var pageScale = 1 / window.devicePixelRatio;
+  document.querySelector('meta[name="viewport"]').setAttribute('content','initial-scale=' + pageScale + ', maximum-scale=' + pageScale + ', minimum-scale=' + pageScale + ', user-scalable=no');
+  document.documentElement.style.fontSize = document.documentElement.clientWidth / 7.5 + 'px'
+}
+
+startResponse()
+
+window.onresize = function () {
+  startResponse()
+}
+
 
 // 为socket.io设置别名
 var socketHostName = document.location.hostname
