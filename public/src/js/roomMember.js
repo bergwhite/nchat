290a700c34a5a8155aa9a99f9a60a09844d3f002
bergwhite <bergwhites@gmail.com>
2017-07-21 21:22:28 +0800

    support online list for room

--- public/src/js/roomMember.js
+++ public/src/js/roomMember.js
@@ -1,3 +1,5 @@
+const roomMember = document.getElementsByClassName('room-member-list-wrap')[0]
+const currentUrlOrigin = document.location.origin
 const socketScriptTag = document.createElement('script')
 const socketScriptSrc = `http://${document.location.hostname}:8087/socket.io/socket.io.js`
 socketScriptTag.src = socketScriptSrc
@@ -8,6 +10,24 @@ socketScriptTag.onload = () => {
   const socketURI = `http://${socketHostName}:8087/`
   const socket = io(socketURI)
   socket.on('user list res', (v) => {
-    console.log(v)
+    renderRoomMemberDOM(v)
   })
+}
+
+function insertToRoomMemberDOM({img, name}){
+  const roomMemberTemplate = `
+    <li>
+      <a class="room-member-list animated fadeIn" href="${currentUrlOrigin}/user/${name}">
+        <div class="room-member-info">
+          <img class="room-member-img" src="${img}">
+          <span class="room-member-name">${name}</span>
+        </div>
+      </a>
+    </li>
+  `
+  roomMember.innerHTML += roomMemberTemplate
+}
+
+function renderRoomMemberDOM(v){
+  v.map((e, i)=>setTimeout(() => insertToRoomMemberDOM(e),200 * i))
 }
