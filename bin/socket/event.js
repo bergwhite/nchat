a67286df751e4dd649ad47d78ab7ed9ddd44eda5
bergwhite <bergwhites@gmail.com>
2017-07-26 23:19:42 +0800

    publish v3.0.18 only pull 100 messes for the room

--- bin/socket/event.js
+++ bin/socket/event.js
@@ -73,7 +73,7 @@ const event = function (chatData, chatMethod, port) {
     socket.on('room id res', (currentRoomName) => {
 
       // 读取当前房间的聊天信息
-      mess.find({'room': currentRoomName}, (err, data) =>  {
+      mess.find({'room': currentRoomName}).limit(100).exec((err, data) =>  {
         console.log('room data ready / ' + (data.lenth !== 0))
         socket.emit('mess show res', data)
       })
